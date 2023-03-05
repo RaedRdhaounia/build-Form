1243cbc3d4bf277233ce606da6433c4c12a0a210
raed rdhaounia <100320828+RaedRdhaounia@users.noreply.github.com>
2023-03-06 00:50:30 +0100

    updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../store/store';
-import "@tailwindcss/ui/dist/tailwind-ui.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
