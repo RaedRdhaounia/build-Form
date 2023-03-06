ac2565df302ac819feeef1a3b840d135e49e3727
raed rdhaounia <100320828+RaedRdhaounia@users.noreply.github.com>
2023-03-06 02:32:08 +0100

    documentation
    create documentation components, page execute and update alias

---
+++ src/components/view/documentation/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="lg:pr-4">
+            <div className="lg:max-w-lg">
+              <h5 className="text-2xl text-center font-semibold leading-relaxed text-indigo-600"> {"🦸____ CREATE FASTER ____🦸"} </h5>
+              <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better Practice ✍</h1>
+              <p className="mt-6 text-xl leading-8 text-gray-700">
+                Form-build Application is a strong form list creator that allowed you to create a strong, flexible and strong form based on amazing components created with modern frameworks
+              </p>
+            </div>
+          </div>
+        </div>
+  )
+}
