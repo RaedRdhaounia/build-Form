db9e7de22093ceaeb7926d1e9c56031bbdd963a9
raed rdhaounia <100320828+RaedRdhaounia@users.noreply.github.com>
2023-03-07 10:00:13 +0100

    fix error errors

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -1,4 +1,4 @@
-import { CheckBoxState, RadioButtonState } from "./types/interfaces"
+import { CheckBoxState, InputFieldState, RadioButtonState } from "./types/interfaces"
 import { BlockP } from "./types/types"
 
 export const radiobottonInfo :RadioButtonState  = {
@@ -8,8 +8,8 @@ export const radiobottonInfo :RadioButtonState  = {
   options: ["option1", "option2", "option3"], 
   name: ""
 }
-export const checkBoxInfo :CheckBoxState = { id:"id", label: "checkbox label" , description:"description for checkbox" , value:false }
-export const textInputInfo = { id:"id", label: "input text label"  , name : " text input name"                 ,                                            }
+export const checkBoxInfo :CheckBoxState    = { id:"id", label: "checkbox label" , description:"description for checkbox" , value:false }
+export const textInputInfo :InputFieldState = { id:"id", label: "input text label"  , value : " text input name"                 ,                                            }
 export const selectInputInfo = { id:"id", label: "radio botton label"                                            , options: ["option1", "option2", "option3"] }
 export const blockInfo1 :BlockP = { id:"id", label: "block label 1", description:"description for block 1 as an example", fields:[ {type:"text" , id:"id"}, {type:"select"  , id:"id"} ]}
 export const blockInfo2 :BlockP = { id:"id", label: "block label 2", description:"description for block 2 as an example", fields:[ {type:"radio", id:"id"}, {type:"checkbox", id:"id"} ]}
