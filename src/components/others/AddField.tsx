1070a50edb94b6f7022108f5404897d5a7824ea0
raed rdhaounia <100320828+RaedRdhaounia@users.noreply.github.com>
2023-03-08 05:51:43 +0100

    mobile functionality

--- src/components/others/AddField.tsx
+++ src/components/others/AddField.tsx
@@ -3,7 +3,9 @@ export default function AddFiled(props: {func?: () => void}) {
     return (
       <div
         onClick={func}
-        className="bg-gray-50 px-4 py-3 text-right sm:px-6"
+        className="bg-gray-50 px-4 py-3 text-center items-center sm:px-6"
+        style={{width: "inherit", display:"contents"}} 
+
       >
         <button
           className="inline-flex justify-center rounded-md bg-purple-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
