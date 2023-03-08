8f0bd2eb353165f9d8b23120b4def681d7556840
raed rdhaounia <100320828+RaedRdhaounia@users.noreply.github.com>
2023-03-08 09:37:24 +0100

    clean up utilities folder files

---
+++ src/components/utilities/panel/index.ts
@@ -0,0 +1,6 @@
+import BlockButton from "./BlockButton";
+import SelectedItem from "./SelectedItem";
+import SelectFiled from "./SelectFiled";
+import Title from "./Title";
+
+export { BlockButton, SelectFiled, SelectedItem, Title}
